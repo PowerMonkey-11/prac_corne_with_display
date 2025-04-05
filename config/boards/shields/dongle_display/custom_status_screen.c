68e2e0fd89910e66c1372048ab7241f40187493e
PowerMonkey-11 <zean01334@gmail.com>
2025-04-05 22:05:12 +0800

    Update custom_status_screen.c

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -54,17 +54,22 @@ lv_obj_t *zmk_display_status_screen() {
 #endif
 
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    // lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 2, -18);
     lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
 
-    zmk_widget_peripheral_battery_status_init(&left_battery_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&left_battery_widget), LV_ALIGN_TOP_RIGHT, 0, 0);  // Adjust position
+    // Initialize left battery widget only if not already initialized
+    if (!left_battery_widget.obj) {
+        zmk_widget_peripheral_battery_status_init(&left_battery_widget, screen);
+        lv_obj_align(zmk_widget_peripheral_battery_status_obj(&left_battery_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+    }
     
-    if (!right_battery_widget.obj) {  // Only initialize right battery widget if not already initialized
+    // Initialize right battery widget only if not already initialized
+    if (!right_battery_widget.obj) {
         zmk_widget_peripheral_battery_status_init(&right_battery_widget, screen);
         lv_obj_align_to(zmk_widget_peripheral_battery_status_obj(&right_battery_widget),
                         zmk_widget_peripheral_battery_status_obj(&left_battery_widget),
                         LV_ALIGN_OUT_BOTTOM_RIGHT, 0, 2);
     }
+
     return screen;
 }
+
