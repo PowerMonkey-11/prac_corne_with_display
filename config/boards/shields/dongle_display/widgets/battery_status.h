9774fbeff7d7a0d99c459a52a8d4f4162f0a4d5f
PowerMonkey-11 <zean01334@gmail.com>
2025-04-06 13:13:48 +0800

    Update battery_status.h

--- config/boards/shields/dongle_display/widgets/battery_status.h
+++ config/boards/shields/dongle_display/widgets/battery_status.h
@@ -12,9 +12,7 @@
 struct zmk_widget_peripheral_battery_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    uint8_t source; // <-- Add this
 };
-
 
 int zmk_widget_peripheral_battery_status_init(struct zmk_widget_peripheral_battery_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_peripheral_battery_status_obj(struct zmk_widget_peripheral_battery_status *widget);
