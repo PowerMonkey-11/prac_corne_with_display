a5495a666f0a60bd69e7c780261f5c3e03e17e79
PowerMonkey-11 <zean01334@gmail.com>
2025-04-06 13:02:46 +0800

    Update custom_status_screen.c

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -57,17 +57,16 @@ lv_obj_t *zmk_display_status_screen() {
     // lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 2, -18);
     lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
 
-    left_battery_widget.source = 0;  // Source 0 = Left
+    // Set source before initialization
+    left_battery_widget.source = 0;  // For the left battery
     zmk_widget_peripheral_battery_status_init(&left_battery_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&left_battery_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&left_battery_widget), LV_ALIGN_TOP_LEFT, 0, 0);  // Top-left positioning
     
-    right_battery_widget.source = 1;  // Source 1 = Right
+    right_battery_widget.source = 1;  // For the right battery
     zmk_widget_peripheral_battery_status_init(&right_battery_widget, screen);
-    lv_obj_align_to(
-        zmk_widget_peripheral_battery_status_obj(&right_battery_widget),
-        zmk_widget_peripheral_battery_status_obj(&left_battery_widget),
-        LV_ALIGN_OUT_BOTTOM_RIGHT,
-        0, 2
-    );
+    lv_obj_align_to(zmk_widget_peripheral_battery_status_obj(&right_battery_widget),
+                    zmk_widget_peripheral_battery_status_obj(&left_battery_widget),
+                    LV_ALIGN_OUT_BOTTOM_RIGHT, 0, 2);  // Positioned under left battery
+
     return screen;
 }
